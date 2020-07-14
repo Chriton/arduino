cce663ddb30256c24706d6446ab73558dbee0373
Doru Muntean <doru.muntean@netcentric.biz>
2020-07-15 00:41:57 +0300

    added support for CH340 boards

--- demos/serial.js
+++ demos/serial.js
@@ -24,6 +24,7 @@ var serial = {};
       { 'vendorId': 0x2341, 'productId': 0x8056 }, // Arduino MKR Vidor 4000
       { 'vendorId': 0x2341, 'productId': 0x8057 }, // Arduino NANO 33 IoT
       { 'vendorId': 0x239A }, // Adafruit Boards!
+      { 'vendorId': 0x1a86 }, // Boards with CH340
     ];
     return navigator.usb.requestDevice({ 'filters': filters }).then(
       device => new serial.Port(device)
